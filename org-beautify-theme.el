df6a1114fda313e1689363e196c8284fbe2a2738
Jonathan Arkell <joarkell@cisco.com>
2017-09-08 16:18:50 -0600

    Ugly box fix
    Add a variable to get around the issue of ugly boxes appearing for
    some people

--- org-beautify-theme.el
+++ org-beautify-theme.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2014-2017 Jonathan Arkell
 
 ;; Author: Jonathan Arkell <jonnay@jonnay.net>
-;; Version: 0.3.2
+;; Version: 0.4
 ;; Created: 5 Oct 2012
 ;; Keywords: org theme
 
@@ -44,7 +44,13 @@
 ;;   still be active with the old theme.  Just unload org-beautify-theme
 ;;   and then reload it, and everything will be fine again.
 ;; 
+;;   If you still get really ugly headlines, customize the
+;;   ~org-beautify-theme-use-box-hack~ variable and set it to nil (false).
+;; 
 ;; * Changelog
+;;    - v0.4 :: [2017-09-08]
+;;      - Add org-beautify-theme-use-box-hack to allow the user to 
+;;        fix ugly boxes.  
 ;;    - v0.3.2 :: [2017-08-29]
 ;;      - Update License
 ;;    - v0.3.1 :: [2016-10-19]
@@ -58,12 +64,18 @@
 ;;    - v0.1.1 :: [2014-01-06]
 ;;      - Fix checkboxes
 ;;    - v0.1 :: First Release
-;; 	 - Make the colors suck a lot less, and the buffers look a lot nicer.
+;;      - Make the colors suck a lot less, and the buffers look a lot nicer.
 ;; 
 
 ;;; Code:
 
 (deftheme org-beautify "Sub-theme to beautify org mode")
+
+(defcustom org-beautify-theme-use-box-hack 't
+  "Use a 3 pixel box with the background color to add spacing.
+Note that this has a side effect that can make the theme look
+really bad under some circumstances."
+  :type 'boolean)
 
 (let* ((sans-font (cond ((x-list-fonts "Lucida Grande") '(:font "Lucida Grande"))
                         ((x-list-fonts "Verdana") '(:font "Verdana"))
@@ -74,7 +86,9 @@
        (headline `(:inherit default :foreground ,base-font-color))
        (primary-color (face-foreground 'mode-line nil))
        (secondary-color (face-background 'secondary-selection nil 'region))
-       (padding `(:line-width 5 :color ,background-color))
+       (padding (if org-beautify-theme-use-box-hack
+                    `(:line-width 5 :color ,background-color)
+                    nil))
        (org-highlights `(:foreground ,base-font-color :background ,secondary-color)))
   (custom-theme-set-faces 'org-beautify
                           `(org-agenda-structure ((t (:inherit default ,@sans-font :height 2.0 :underline nil))))
@@ -85,7 +99,7 @@
                           `(org-level-4 ((t ,headline)))
                           `(org-level-3 ((t (,@headline  :box ,padding))))
                           `(org-level-2 ((t (,@headline ,@sans-font :height 1.25 :box ,padding))))
-                          `(org-level-1 ((t (,@headline ,@sans-font :height 1.5 :box ,padding ))))
+                          `(org-level-1 ((t (,@headline ,@sans-font :height 1.5  :box ,padding ))))
                           `(org-document-title ((t (:inherit org-level-1 :height 2.0 :underline nil :box ,padding))))
 
                           `(org-block ((t (:foreground ,base-font-color :background ,background-color :box nil))))
